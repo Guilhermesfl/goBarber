1c0e6adae96aad644fc43772670ce705f17cbc1f
Guilhermesfl <guilherme@loopce.com>
2019-10-30 16:02:12 -0300

    :wrench: Added initial webpack configuration

---
+++ frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", //Responsible for altering JS functions that browser does not understand
+    "@babel/preset-react" // Transforms React things
+  ],
+};
